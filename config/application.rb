67cdcefd625c9a019707a604cee8d2c9e1f289e6
Keisuke Nagashima <energyrevo21@gmail.com>
2019-07-25 14:58:18 +0900

    Add config for rails generator

---
+++ config/application.rb
@@ -0,0 +1,35 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TodoServer
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # 不要なview/test関連ファイルのgenrarateを防ぐ
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.template_engine false
+
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+	      helper_specs: false,
+       	routing_specs: false,
+	      controller_specs: false,
+	      request_specs: true,
+	      model_specs: true
+    end
+  end
+end
